2a66b2465165a005129e6e369269e9f378827ff6
Artur-Niezgoda <110778550+Artur-Niezgoda@users.noreply.github.com>
2022-11-09 13:35:50 +0100

    add scrape price function

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
+header = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding" : "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.9,pl-PL;q=0.8,pl;q=0.7",
+        "Connection": "keep-alive",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/107.0.0.0 Safari/537.36"
+}
+price_limit = 15
+URL = "https://a.co/d/5iUKt2H"
+response = requests.get(url=URL, headers=header)
+soup = BeautifulSoup(response.text, 'html.parser')
+price = float(soup.find("span", class_=["a-offscreen"]).get_text().split("$")[1])
+print(price)
